53362d3231619c3888f5ded8b6e747abaa3b062e
Henning Bredel <h.bredel@52north.org>
2023-05-15 14:53:25 +0200

    Apply thuenen atlas patch
    Includes
    
    - Update model including a 'public' indicator
    - view shows atlas on public or for authenticated user
    - sorts atlas list by title

--- src/atlas/views.py
+++ src/atlas/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.views.generic import DetailView, ListView
 from django.views.generic.base import TemplateView
+from django.utils.translation import ugettext as _
+from django.http import HttpResponse
 
 from .models import Atlas, AtlasCustomAppListing
 
@@ -12,9 +14,12 @@ class AtlasDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["customApps"] = AtlasCustomAppListing.objects.filter(
-            atlas=context["atlas"]
-        )
+        if (self.object.public or self.request.user.is_authenticated):
+            context["customApps"] = AtlasCustomAppListing.objects.filter(
+                atlas=context["atlas"]
+            )
+        else:
+            return HttpResponse(_("Not allowed"), status=403)
         return context
 
 
