40675470881a13b637ac336516353e1747cba1fd
Henning Bredel <h.bredel@52north.org>
2023-12-06 13:18:42 +0100

    Rename settings.py
    closes #10

--- geonode/geonode/settings_override.py
+++ geonode/geonode/settings_override.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
+
 # load the defaults settings
 from geonode.settings import *
 from geonode.settings import TEMPLATES, INSTALLED_APPS, IMPORTER_HANDLERS
 
 
-SITENAME = os.getenv("SITENAME", 'thuenen_atlas')
+SITENAME = os.getenv("SITENAME", "thuenen_atlas")
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
 
@@ -15,69 +16,86 @@ LOCAL_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Additional directories which hold static files
 # - Give priority to local ones
-TEMPLATES[0]['DIRS'].insert(0, "/usr/src/geonode/templates")
-loaders = ['django.template.loaders.filesystem.Loader', 'django.template.loaders.app_directories.Loader']
-TEMPLATES[0]['OPTIONS']['loaders'] = loaders
-TEMPLATES[0].pop('APP_DIRS', None)
+TEMPLATES[0]["DIRS"].insert(0, "/usr/src/geonode/templates")
+loaders = [
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+]
+TEMPLATES[0]["OPTIONS"]["loaders"] = loaders
+TEMPLATES[0].pop("APP_DIRS", None)
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d '
-                      '%(thread)d %(message)s'
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s %(process)d "
+            "%(thread)d %(message)s"
         },
-        'simple': {
-            'format': '%(message)s',
+        "simple": {
+            "format": "%(message)s",
         },
     },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'ERROR',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "handlers": {
+        "console": {
+            "level": "ERROR",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
         },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler',
-        }
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
     },
     "loggers": {
         "django": {
-            "handlers": ["console"], "level": "ERROR", },
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
         "geonode": {
-            "handlers": ["console"], "level": "INFO", },
+            "handlers": ["console"],
+            "level": "INFO",
+        },
         "geoserver-restconfig.catalog": {
-            "handlers": ["console"], "level": "ERROR", },
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
         "owslib": {
-            "handlers": ["console"], "level": "ERROR", },
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
         "pycsw": {
-            "handlers": ["console"], "level": "ERROR", },
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
         "celery": {
-            "handlers": ["console"], "level": "DEBUG", },
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
         "mapstore2_adapter.plugins.serializers": {
-            "handlers": ["console"], "level": "DEBUG", },
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
         "geonode_logstash.logstash": {
-            "handlers": ["console"], "level": "DEBUG", },
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
     },
 }
 
 IMPORTER_HANDLERS = (
-    'importer_datapackage.handlers.datapackage.handler.DataPackageFileHandler',
+    "importer_datapackage.handlers.datapackage.handler.DataPackageFileHandler",
     *IMPORTER_HANDLERS,
 )
 
-INSTALLED_APPS += ("atlas",
-                   "externalapplications",
-                   "importer_datapackage",
-                   "thuenen_app")
+INSTALLED_APPS += (
+    "atlas",
+    "externalapplications",
+    "importer_datapackage",
+    "thuenen_app",
+)
 
 # LDAP
 # WE DO NOT USE CONTRIB APP BUT
@@ -88,21 +106,21 @@ INSTALLED_APPS += ("atlas",
 # LDAP conf:
 
 from django_auth_ldap import config as ldap_config
+
 # from geonode_ldap.config import GeonodeNestedGroupOfNamesType
 import ldap
 
-LDAP_ENABLED = ast.literal_eval(os.getenv('LDAP_ENABLED', 'False'))
+LDAP_ENABLED = ast.literal_eval(os.getenv("LDAP_ENABLED", "False"))
 
 # enable logging
 import logging
-logger = logging.getLogger('django_auth_ldap')
+
+logger = logging.getLogger("django_auth_ldap")
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
 # add both standard ModelBackend auth and django_auth_ldap.backend.LDAPBackend auth
-AUTHENTICATION_BACKENDS += (
-    'django_auth_ldap.backend.LDAPBackend',
-)
+AUTHENTICATION_BACKENDS += ("django_auth_ldap.backend.LDAPBackend",)
 
 # django_auth_ldap configuration
 AUTH_LDAP_SERVER_URI = os.getenv("LDAP_SERVER_URL")
@@ -113,12 +131,12 @@ AUTH_LDAP_BIND_PASSWORD = os.getenv("LDAP_BIND_PASSWORD")
 AUTH_LDAP_USER_SEARCH = ldap_config.LDAPSearch(
     os.getenv("LDAP_USER_SEARCH_DN"),
     ldap.SCOPE_SUBTREE,
-    os.getenv("LDAP_USER_SEARCH_FILTERSTR")
+    os.getenv("LDAP_USER_SEARCH_FILTERSTR"),
 )
 AUTH_LDAP_USER_ATTR_MAP = {
     "first_name": "givenName",
     "last_name": "sn",
-    "email": "mail"
+    "email": "mail",
 }
 
 # GROUPS
