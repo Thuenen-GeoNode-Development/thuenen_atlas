878c509ff2f2b0bbc41c6b54eb24ddc1a85f0f22
Henning Bredel <881756+ridoo@users.noreply.github.com>
2024-01-30 10:51:46 +0100

    Upgrades geonode to upstream 4.2.1 (#18)
    * Migrate to GeoNode 4.2.1
    * Update submodule(s)
    
    Closes #17

--- .devcontainer/docker/geonode/custom_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-oauth_config="/geoserver_data/data/security/filter/geonode-oauth2/config.xml"
-redirect_uri="${GEOSERVER_PUBLIC_LOCATION}"geoserver/index.html
-
-sed -i "s|<redirectUri>.*</redirectUri>|<redirectUri>$redirect_uri</redirectUri>|g" $oauth_config
-
-./entrypoint.sh sleep infinity
