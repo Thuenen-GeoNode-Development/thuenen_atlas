d78559785b0963f228c6b51233467a7c03143af3
Henning Bredel <h.bredel@52north.org>
2024-08-27 10:36:00 +0200

    Trust geonode-mapstore-client dev server when DEBUG

--- geonode/geonode/settings_override.py
+++ geonode/geonode/settings_override.py
@@ -20,7 +20,10 @@ SITENAME = os.getenv("SITENAME", "thuenen_atlas")
 X_FRAME_OPTIONS = "SAMEORIGIN"
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None if DEBUG else "same-origin"
 # required for geonode-mapstore-client development
-CSRF_TRUSTED_ORIGINS = ["http://172.18.0.1:8001"] if DEBUG else []
+CSRF_TRUSTED_ORIGINS = [
+    "http://172.18.0.1:8001",
+    "http://localhost:8081"
+] if DEBUG else []
 CORS_ALLOWED_ORIGINS = ast.literal_eval(os.getenv("CORS_ALLOWED_ORIGINS", "[]"))
 
 
